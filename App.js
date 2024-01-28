fa23ec008cbe27a68cbcfbcacf50fde0a24c95fe
jhamid <jhamidridiaz@gmail.com>
2024-01-28 05:12:07 -0500

    InterfasLogin

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './login';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
+      <LoginScreen />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
